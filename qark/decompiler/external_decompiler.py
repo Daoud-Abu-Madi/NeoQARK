4e4b6117dafd3630c140e84ca71be2cdddaa7877
DaoudM2003 <djabumadi3@gmail.com>
2025-04-08 08:32:38 +0300

    Update external_decompiler.py

--- qark/decompiler/external_decompiler.py
+++ qark/decompiler/external_decompiler.py
@@ -1,20 +1,20 @@
 import abc
 import os
 
-LIB_PATH = os.path.join(os.path.dirname(os.path.abspath(_file_)), "..", "lib")
+LIB_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "lib")
 PATH_TO_DECOMPILERS = os.path.join(LIB_PATH, "decompilers")
 
 
 class ExternalDecompiler(abc.ABC):  # استخدام abc.ABC بدلاً من _meta_ = abc.ABCMeta
-    def _init_(self, name, path_to_decompiler, command=None):
+    def __init__(self, name, path_to_decompiler, command=None):
         self.name = name.lower()
         self.path_to_decompiler = path_to_decompiler
         self.command = command
 
 
 class CFR(ExternalDecompiler):
-    def _init_(self):
-        super()._init(  # استخدام super() بدلاً من ExternalDecompiler.init_
+    def __init__(self):
+        super().__init__(
             name="cfr",
             path_to_decompiler=os.path.join(PATH_TO_DECOMPILERS, "cfr_0_124.jar"),
             command="java -jar {path_to_decompiler} {jar} --outputdir {build_directory}/cfr"
@@ -22,8 +22,8 @@ class CFR(ExternalDecompiler):
 
 
 class Procyon(ExternalDecompiler):
-    def _init_(self):
-        super()._init_(
+    def __init__(self):
+        super().__init__(
             name="procyon",
             path_to_decompiler=os.path.join(PATH_TO_DECOMPILERS, "procyon-decompiler-1.0.jar"),
             command="java -jar {path_to_decompiler} {jar} -o {build_directory}/procyon"
@@ -31,8 +31,8 @@ class Procyon(ExternalDecompiler):
 
 
 class Fernflower(ExternalDecompiler):
-    def _init_(self):
-        super()._init_(
+    def __init__(self):
+        super().__init__(
             name="fernflower",
             path_to_decompiler=os.path.join(PATH_TO_DECOMPILERS, "fernflower.jar"),
             command="java -jar {path_to_decompiler} -ren=1 {jar} {build_directory}/fernflower"
